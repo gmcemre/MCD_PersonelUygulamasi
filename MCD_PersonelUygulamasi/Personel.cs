20bde51716590a9fec1bd2796840af25e85ea3a7
emre gemici <gmcc.emree@gmail.com>
2021-12-01 16:29:52 +0300

    code changed

---
+++ MCD_PersonelUygulamasi/Personel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_PersonelUygulamasi
+{
+    public class Personel
+    {
+        public static string domainAdres;
+        public string Isim { get; set; }
+        public string  Soyisim { get; set; }
+
+        private string _emailAdres;
+        public string  Emailadres 
+        {
+            get
+            {
+                return this._emailAdres;
+            }
+            set 
+            {
+                this._emailAdres = value.ToLower() + "@" + Personel.domainAdres;
+            } 
+        }
+
+        public int Cinsiyet { get; set; }
+
+        public Personel()
+        {
+            Console.WriteLine("Her örneklemede çalışan metot");
+        }
+        
+        static Personel()
+        {
+            domainAdres = "firmam.com";
+        }
+    }
+}
