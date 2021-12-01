ab68747d695de223b302da294cde07c02b380b53
emre gemici <gmcc.emree@gmail.com>
2021-12-01 17:13:11 +0300

    code changed

--- MCD_PersonelUygulamasi/Program.cs
+++ MCD_PersonelUygulamasi/Program.cs
@@ -10,7 +10,22 @@ namespace MCD_PersonelUygulamasi
     {
         static void Main(string[] args)
         {
+            Personel P1 = new Personel();
+            P1.Isim = "Emre";
+            P1.Soyisim = "Gemici";
+            P1.Emailadres = "gmcc.emree";
+            P1.Cinsiyet = 717770001; //Bay => 717770002-Bayan
 
+            Personel P2 = new Personel();
+            P2.Isim = "Ali";
+            P2.Soyisim = "Gel";
+            P2.Emailadres = "ali.gel";
+            P2.Cinsiyet = 717770001; //Bay => 717770002-Bayan
+
+            Helper.emailGonder("ik@firmam.com", "Yeni Personel Bilgilendirme", P1.Isim + P1.Soyisim);
+            Helper.emailGonder("ik@firmam.com", "Yeni Personel Bilgilendirme", P2.Isim + P2.Soyisim);
+
+            Console.ReadKey ();
         }
     }
 }
